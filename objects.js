5d5b32f12e50fb3a2e2fcc9739a45a1a0e467da2
Patt Sookmark <pijiya@sookmark.com>
2022-09-22 14:05:13 -0600

    Add answer to object

--- objects.js
+++ objects.js
@@ -6,31 +6,37 @@
 // file in your Terminal.
 
 foods = {
-  apples: 23, 
-  grapes: 507, 
+  apples: 23,
+  grapes: 507,
   eggs: 48
 }
 
-// Write code that prints all of the 'keys' of the foods variable 
+// Write code that prints all of the 'keys' of the foods variable
 // you created above:
 // YOUR CODE HERE
+console.log(Object.keys(foods));
 
-// Write code that prints all of the 'values' of the foods variable 
+
+// Write code that prints all of the 'values' of the foods variable
 // you created above:
 // YOUR CODE HERE
+console.log(Object.values(foods));
 
-// Write code that prints the value of the second food of the foods variable 
+// Write code that prints the value of the second food of the foods variable
 // you created above:
 // YOUR CODE HERE
+console.log(foods.grapes);
 
-// Write code that adds a food to the foods object. 
+// Write code that adds a food to the foods object.
 // Then, print the updated object:
 // YOUR CODE HERE
+foods.bananas = 33;
+console.log(foods);
 
-
-#-------------------
+// #-------------------
 // Part 2: Email
-#-------------------
+// #-------------------
+
 
 
 // Think about all the pieces of information associated with one single email in your inbox.
@@ -38,28 +44,36 @@ foods = {
 
 // Declare a variable that stores an object. Each key should be an attribute of an email and each
 // value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
+var email = {
+  senderName: "Lalisa Pa",
+  senderAddress : "Main St",
+  subject: "Update status of your order",
+  body:"ipsum leren",
+  time: "5.20pm",
+  date: "Jan 24,2022",
+  };
 
 // Write code that logs your email object to the terminal.
+console.log(email);
 
-
-// Write code that logs all of the 'keys' of the email object 
+// Write code that logs all of the 'keys' of the email object
 // you created above:
 // YOUR CODE HERE
+console.log(Object.keys(email));
 
-// Write code that logs all of the 'values' of the email object 
+// Write code that logs all of the 'values' of the email object
 // you created above:
 // YOUR CODE HERE
+console.log(Object.values(email));
 
-
-#-------------------
+// #-------------------
 // Part 3: Many Emails - CHALLENGE!
-#-------------------
+// #-------------------
 
 // LONG EXAMPLE:
 // Now that we've learned about Objects AND Arrays, we can combine them.
 
 // Check out the following example of an array of Instagram posts:
-
 
 posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
@@ -76,7 +90,8 @@ posts = [
     timestamp: "4:37 PM August 13, 2019",
     number_likes: 0,
     comments: []
-  }, 
+  },
+
   {
     imageSrc: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -90,12 +105,41 @@ console.log(posts);
 console.log(posts[0]);
 
 
-// The code snippet above shows an Array with 2 elements. Each element in the Array is a 
-// Objects. Each of those Objects has 4 key-value pairs. This may LOOK 
+// The code snippet above shows an Array with 2 elements. Each element in the Array is a
+// Objects. Each of those Objects has 4 key-value pairs. This may LOOK
 // a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 // good to have some exposure before going into Mod 1.
 
 
-// YOU DO: Create an array of at least 3 EMAIL Objects, using the same 
+// YOU DO: Create an array of at least 3 EMAIL Objects, using the same
 // key-value pairs you used in your email Object above.
 // Then, print the email Array to the Terminal.
+var emails = [
+  {
+    senderName: "Jennie LA",
+    senderAddress : "Last St",
+    subject: "Update status of your order",
+    body:"ipsum leren",
+    time: "5.20pm",
+    date: "Jan 24,2022"
+  },
+
+  {
+    senderName: "Rose BP",
+    senderAddress : "Main St",
+    subject: "Update status of your order",
+    body:"ipsum leren",
+    time: "5.20pm",
+    date: "Jan 24,2022"
+  },
+
+  {
+    senderName: "Jisoo Pa",
+    senderAddress : "Main St",
+    subject: "Update status of your order",
+    body:"ipsum leren",
+    time: "5.20pm",
+    date: "Jan 24,2022"
+  }
+]
+console.log(emails);
